8000171089a5a35ffdd547711bf7c76c0de3cdab
Eddie Saliby <eddysaliby@gmail.com>
2021-10-21 23:33:13 +0200

    Added Snake object

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ FramePerSec = pygame.time.Clock()
 
 
 BLACK = (0, 0, 0)
-DISPLAYSURF = pygame.display.set_mode((400, 600))
+DISPLAYSURF = pygame.display.set_mode((400, 400))
 DISPLAYSURF.fill(BLACK)
 pygame.display.set_caption("Game")
 
@@ -27,6 +27,33 @@ class Apple(pygame.sprite.Sprite):
         surface.blit(self.image, self.rect)
 
 
+class Snake(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("apple.png")
+        self.image = pygame.transform.scale(self.image, (10, 10))
+        self.rect = self.image.get_rect()
+        self.rect.center = (0, 0)
+
+    def update(self):
+        pressed_keys = pygame.key.get_pressed()
+        if self.rect.top < 400:
+            if pressed_keys[K_UP]:
+                self.rect.move_ip(0, -5)
+        if self.rect.bottom < 400:
+            if pressed_keys[K_DOWN]:
+                self.rect.move_ip(0, 5)
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < 400:
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+
 apples = []
 xpos = []
 
@@ -38,7 +65,7 @@ for i in range(15):
 
 print(apples)
 
-
+E1 = Snake()
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -47,5 +74,7 @@ while True:
     DISPLAYSURF.fill(BLACK)
     for a in apples:
         a.draw(DISPLAYSURF)
+    E1.update()
+    E1.draw(DISPLAYSURF)
     pygame.display.update()
     FramePerSec.tick(FPS)
