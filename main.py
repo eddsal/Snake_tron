6973d75e44eb4f0a31ddf4c2003f895cfc9f1725
Eddie Saliby <eddysaliby@gmail.com>
2021-12-24 08:09:07 +0100

    [backend] - Changed game structure, add collistion , increale length

--- main.py
+++ main.py
@@ -1,85 +1,148 @@
 import pygame
-import sys
 from pygame.locals import *
+import time
 import random
 
-pygame.init()
-
-FPS = 60
-FramePerSec = pygame.time.Clock()
+SIZE = 40
 
 
-BLACK = (0, 0, 0)
-DISPLAYSURF = pygame.display.set_mode((400, 400))
-DISPLAYSURF.fill(BLACK)
-pygame.display.set_caption("Game")
+class Apple:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.image = pygame.image.load("./apple.jpeg").convert()
+        self.x = 20
+        self.y = 20
+
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+
+    def move(self):
+        self.x = random.randint(1, 25)*SIZE
+        self.y = random.randint(1, 20)*SIZE
 
 
-class Apple(pygame.sprite.Sprite):
-    def __init__(self, xpos):
-        super().__init__()
-        self.image = pygame.image.load("apple.png")
-        self.image = pygame.transform.scale(self.image, (20, 20))
-        self.rect = self.image.get_rect()
-        self.rect.center = [x for x in xpos]
+class Snake:
+    def __init__(self, parent_screen, length):
+        self.parent_screen = parent_screen
+        self.image = pygame.image.load("./block.jpeg").convert()
+        self.direction = 'down'
 
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
+        self.length = length
+        self.x = [40]*length
+        self.y = [40]*length
+
+    def move_left(self):
+        self.direction = 'left'
+
+    def move_right(self):
+        self.direction = 'right'
+
+    def move_up(self):
+        self.direction = 'up'
+
+    def move_down(self):
+        self.direction = 'down'
+
+    def walk(self):
+        # update body
+        for i in range(self.length-1, 0, -1):
+            self.x[i] = self.x[i-1]
+            self.y[i] = self.y[i-1]
+
+        # update head
+        if self.direction == 'left':
+            self.x[0] -= SIZE
+        if self.direction == 'right':
+            self.x[0] += SIZE
+        if self.direction == 'up':
+            self.y[0] -= SIZE
+        if self.direction == 'down':
+            self.y[0] += SIZE
+
+        self.draw()
+
+    def draw(self):
+        self.parent_screen.fill((110, 110, 5))
+
+        for i in range(self.length):
+            self.parent_screen.blit(self.image, (self.x[i], self.y[i]))
+        pygame.display.flip()
+
+    def increase_length(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
 
 
-class Snake(pygame.sprite.Sprite):
+class Game:
     def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("apple.png")
-        self.image = pygame.transform.scale(self.image, (10, 10))
-        self.rect = self.image.get_rect()
-        self.rect.center = (0, 0)
+        pygame.init()
+        self.surface = pygame.display.set_mode((1000, 800))
+        self.snake = Snake(self.surface, 2)
+        self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
 
-    def update(self):
-        pressed_keys = pygame.key.get_pressed()
-        if self.rect.top < 400:
-            if pressed_keys[K_UP]:
-                self.rect.move_ip(0, -5)
-        if self.rect.bottom < 400:
-            if pressed_keys[K_DOWN]:
-                self.rect.move_ip(0, 5)
-        if self.rect.left > 0:
-            if pressed_keys[K_LEFT]:
-                self.rect.move_ip(-5, 0)
-        if self.rect.right < 400:
-            if pressed_keys[K_RIGHT]:
-                self.rect.move_ip(5, 0)
+    def is_collision(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and y1 < y2 + SIZE:
+                return True
+        return False
 
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
+    def is_snake_collision(self, x1, y1,):
+        if x1 and y1:
+            print('collisiion')
+        return False
+
+    def display_score(self):
+        font = pygame.font.SysFont('arial', 30)
+        score = font.render(f"Score: {self.snake.length}", True, (200, 200, 200))
+        self.surface.blit(score, (850, 10))
+
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
+
+        # When snake collision
+        for i in range(3, self.snake.length):
+            if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
+                print("Collision Occurred")
+
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+
+                    if event.key == K_LEFT:
+                        self.snake.move_left()
+
+                    if event.key == K_RIGHT:
+                        self.snake.move_right()
+
+                    if event.key == K_UP:
+                        self.snake.move_up()
+
+                    if event.key == K_DOWN:
+                        self.snake.move_down()
+
+                elif event.type == QUIT:
+                    running = False
+
+            self.play()
+
+            time.sleep(.2)
 
 
-def range_appender(rng, value):
-    xpos = []
-    for i in range(rng):
-        xpos.append(value)
-    return xpos
-
-
-apples = range_appender(10, Apple([random.randint(0, 500), random.randint(0, 500)]))
-E1 = Snake()
-
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-
-    DISPLAYSURF.fill(BLACK)
-    for a in apples:
-        a.draw(DISPLAYSURF)
-    E1.update()
-    E1.draw(DISPLAYSURF)
-    grounded = 0
-    for p in apples:
-        if E1.rect.collidepoint(p.rect.centerx, p.rect.bottom+4):
-            print('collpase')
-            grounded = 1
-            break
-    pygame.display.update()
-    FramePerSec.tick(FPS)
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ utils.py
@@ -0,0 +1,7 @@
+
+def range_appender(rng, value):
+    xpos = []
+    for i in range(rng):
+        print(value, i)
+        xpos.append(value)
+    return xpos
